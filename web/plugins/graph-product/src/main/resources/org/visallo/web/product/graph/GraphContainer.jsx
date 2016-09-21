e08ffcc53a058130584451fbd28beefbda299c93
Jason Harwig <jason@pinepointsoftware.com>
2016-09-21 18:05:53 -0500

    Fix npe

--- web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/GraphContainer.jsx
+++ web/plugins/graph-product/src/main/resources/org/visallo/web/product/graph/GraphContainer.jsx
@@ -13,7 +13,8 @@ define([
             var viewport = state.product.viewports[props.product.id] || { zoom: 1, pan: {x: 0, y: 0 }},
                 selection = state.selection.idsByType,
                 workspaceId = state.workspace.currentId,
-                pixelRatio = state.screen.pixelRatio;
+                pixelRatio = state.screen.pixelRatio,
+                workspaceElements = state.element[workspaceId] || {};
 
             return {
                 ...props,
@@ -21,8 +22,8 @@ define([
                 viewport,
                 pixelRatio,
                 elements: {
-                    vertices: _.pick(state.element[workspaceId].vertices, _.pluck(props.product.extendedData.vertices, 'id')),
-                    edges: _.pick(state.element[workspaceId].edges, _.pluck(props.product.extendedData.edges, 'id'))
+                    vertices: _.pick(workspaceElements.vertices, _.pluck(props.product.extendedData.vertices, 'id')),
+                    edges: _.pick(workspaceElements.edges, _.pluck(props.product.extendedData.edges, 'id'))
                 }
             }
         },
